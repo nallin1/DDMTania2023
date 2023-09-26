86bc67b9494e60c83e255fdc35ed8c745bdb0492
nallin1 <nallinguilherme@gmail.com>
2023-09-26 11:28:36 -0300

    crudfinished

--- crud/lib/home.dart
+++ crud/lib/home.dart
@@ -22,141 +22,139 @@ class Home extends StatelessWidget {
         title: Text("Cadastro"),
       ),
       body: Center(
-          child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(height: 100),
-              Icon(
-                Icons.login,
-                size: 40,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              SizedBox(width: 20),
-              Text(
-                "Cadastro de Aluno",
-                style: TextStyle(fontSize: 30),
-              )
-            ],
-          ),
-          SizedBox(height: 30),
-          // text field
-          Form(
-              key: _loginFormKey,
-              child: Column(
-                children: [
-                  SizedBox(
-                    width: 300,
-                    child: TextFormField(
-                      controller: _nameController,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Digite um nome';
-                        }
-                        return null;
-                      },
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Nome',
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 100),
+                Icon(
+                  Icons.login,
+                  size: 40,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                SizedBox(width: 20),
+                Text(
+                  "Cadastro de Aluno",
+                  style: TextStyle(fontSize: 30),
+                )
+              ],
+            ),
+            SizedBox(height: 30),
+            // text field
+            Form(
+                key: _loginFormKey,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: 300,
+                      child: TextFormField(
+                        controller: _nameController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Digite um nome';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Nome',
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 20),
+                    SizedBox(height: 20),
 
-                  SizedBox(
-                    width: 300,
-                    child: TextFormField(
-                      keyboardType: TextInputType.number,
-                      controller: _raController,
-                      validator: (value) {
-                        if (value == null ||
-                            value.isEmpty ||
-                            value.length < 5 ) {
-                          return 'Digite um ra válido';
-                        }
-                        return null;
-                      },
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'RA',
+                    SizedBox(
+                      width: 300,
+                      child: TextFormField(
+                        keyboardType: TextInputType.number,
+                        controller: _raController,
+                        validator: (value) {
+                          if (value == null ||
+                              value.isEmpty ||
+                              value.length < 5) {
+                            return 'Digite um ra válido';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'RA',
+                        ),
                       ),
                     ),
-                  ),
 
-                  SizedBox(height: 20),
-                  // text field
-                  SizedBox(
-                    width: 300,
-                    child: TextFormField(
-                      controller: _emailController,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Digite um texto';
-                        }
-                        return null;
-                      },
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Email',
+                    SizedBox(height: 20),
+                    // text field
+                    SizedBox(
+                      width: 300,
+                      child: TextFormField(
+                        controller: _emailController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Digite um texto';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Email',
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 20),
-                  // buttom for login
-                  SizedBox(
-                    width: 300,
-                    child: TextFormField(
-                      obscureText: true,
-                      controller: _senhaController,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Digite um texto';
-                        }
-                        return null;
-                      },
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Senha',
+                    SizedBox(height: 20),
+                    // buttom for login
+                    SizedBox(
+                      width: 300,
+                      child: TextFormField(
+                        obscureText: true,
+                        controller: _senhaController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Digite um texto';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Senha',
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 50),
-                  SizedBox(
-                    width: 200,
-                    height: 50,
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            Theme.of(context).colorScheme.primary),
-                      ),
-                      onPressed: () {
-                        if (_loginFormKey.currentState!.validate()) {
-                          nome = _nameController.text;
-                          email = _emailController.text;
-                          senha = _senhaController.text;
-                          ra = int.parse(_raController.text);
-                          Aluno al = new Aluno(ra, nome, email, senha);
-                          alRepository.addAluno(al);
-                          print("Aluno cadastrado com sucesso ! \n");
-                          alRepository.printOneAluno(ra);
-                          alRepository.printAllAlunos();
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => SecondPage()));
-                        }
-                      },
-                      child: const Text(
-                        'Cadastrar',
-                        style: TextStyle(fontSize: 20, color: Colors.white),
+                    SizedBox(height: 50),
+                    SizedBox(
+                      width: 200,
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              Theme.of(context).colorScheme.primary),
+                        ),
+                        onPressed: () {
+                          if (_loginFormKey.currentState!.validate()) {
+                            nome = _nameController.text;
+                            email = _emailController.text;
+                            senha = _senhaController.text;
+                            ra = int.parse(_raController.text);
+                            Aluno al = new Aluno(ra, nome, email, senha);
+                            alRepository.addAluno(al);
+                            print("Aluno cadastrado com sucesso ! \n");
+                            alRepository.printOneAluno(ra);
+                            alRepository.printAllAlunos();
+                            Navigator.pushNamed(context, '/listAlunos');
+                          }
+                        },
+                        child: const Text(
+                          'Cadastrar',
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ))
-        ],
-      )),
+                  ],
+                ))
+          ],
+        ),
+      ),
     );
   }
 }
--- crud/lib/second.dart
+++ crud/lib/second.dart
@@ -16,79 +16,130 @@ class SecondPage extends StatefulWidget {
 class _SecondPageState extends State<SecondPage> {
   final listaAlunos = AlunoRepository.alunos;
 
+  List<Aluno> listaBusca = [];
+  String nomeBusca = "";
+
+  @override
+  void initState() {
+    listaBusca = List.from(listaAlunos);
+    super.initState();
+  }
+
+  void atualizaLista(String nome) {
+    listaBusca = List.from(listaAlunos);
+
+    setState(() {
+      listaBusca = listaAlunos
+          .where((Aluno element) => element.nome.toLowerCase().contains(nome.toLowerCase()))
+          .toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue[200],
-          title: Text("Lista de Alunos"),
-        ),
-        body: Column(
-          children: [
-            Expanded(
-                child: ListView.separated(
-                    shrinkWrap: true,
-                    itemBuilder: (BuildContext context, int index) {
-                      return ListTile(
-                        leading: CircleAvatar(
-                          child: Text(listaAlunos[index].nome[0]),
-                          backgroundColor: Colors.blue[200],
-                        ),
-                        title: Text("Nome: " + listaAlunos[index].nome),
-                        subtitle: Text(
-                          "RA: " + listaAlunos[index].ra.toString() + "\n" + "Email: " + listaAlunos[index].email + "\n" + "Senha: " + listaAlunos[index].senha.toString(),
-                          style: TextStyle(fontSize: 15),
-                        ),
-                        trailing: SizedBox(
-                          width: 70,
-                          child: Row(
-                            children: [
-                              Expanded(
-                                child: IconButton(
-                                    onPressed: () {
-                                      Aluno al = listaAlunos[index];
-                                      AlunoRepository.deleteAluno(al);
-                                      setState(() {});
-                                    },
-                                    icon: Icon(
-                                      Icons.delete,
-                                      color: Colors.black,
-                                    )),
-                              ),
-                              Expanded(
-                                child: IconButton(
-                                    onPressed: () {
-                                      Navigator.push(context,
-                                          MaterialPageRoute(builder: (context) {
-                                        return UpdateAluno(
-                                            aluno: listaAlunos[index],
-                                            indice: index);
-                                      }));
-                                    },
-                                    icon: Icon(
-                                      Icons.edit,
-                                      color: Colors.black,
-                                    )),
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                    separatorBuilder: (context, index) => Divider(
-                          thickness: 2,
-                        ),
-                    itemCount: listaAlunos.length)),
-            // botão para voltar
-            Padding(
-              padding: EdgeInsets.all(30),
-              child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
+      appBar: AppBar(
+        backgroundColor: Colors.blue[200],
+        title: Text("Lista de Alunos"),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.blue[100],
+                  borderRadius: BorderRadius.circular(10)),
+              width: 300,
+              child: Form(
+                child: TextField(
+                  decoration: InputDecoration(
+                    icon: Padding(
+                        padding: EdgeInsets.all(10), child: Icon(Icons.search)),
+                    hintText: "Pesquisar",
+                    border: InputBorder.none,
+                  ),
+                  onChanged: (String nome) {
+                    nomeBusca = nome;
+                    atualizaLista(nomeBusca);
                   },
-                  child: Text("Voltar")),
+                ),
+              ),
             ),
-          ],
-        ));
+          ),
+          Expanded(
+              child: ListView.separated(
+                  shrinkWrap: true,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        child: Text(listaBusca[index].nome[0]),
+                        backgroundColor: Colors.blue[200],
+                      ),
+                      title: Text("Nome: " + listaBusca[index].nome),
+                      subtitle: Text(
+                        "RA: " +
+                            listaBusca[index].ra.toString() +
+                            "\n" +
+                            "Email: " +
+                            listaBusca[index].email +
+                            "\n" +
+                            "Senha: " +
+                            listaBusca[index].senha.toString(),
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      trailing: SizedBox(
+                        width: 70,
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: IconButton(
+                                  onPressed: () {
+                                    Aluno al = listaAlunos[index];
+                                    AlunoRepository.deleteAluno(al);
+                                    atualizaLista(nomeBusca);
+                                    setState(() {});
+                                  },
+                                  icon: Icon(
+                                    Icons.delete,
+                                    color: Colors.black,
+                                  )),
+                            ),
+                            Expanded(
+                              child: IconButton(
+                                  onPressed: () {
+                                    Navigator.push(context,
+                                        MaterialPageRoute(builder: (context) {
+                                      return UpdateAluno(
+                                          aluno: listaBusca[index],
+                                          indice: index);
+                                    }));
+                                  },
+                                  icon: Icon(
+                                    Icons.edit,
+                                    color: Colors.black,
+                                  )),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                  separatorBuilder: (context, index) => Divider(
+                        thickness: 2,
+                      ),
+                  itemCount: listaBusca.length)),
+          // botão para voltar
+          Padding(
+            padding: EdgeInsets.all(30),
+            child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Voltar")),
+          ),
+        ],
+      ),
+    );
   }
 }
